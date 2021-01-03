de8835f35321ae28a4bfa44d96199c757d435f77
alex <alex_kobilianskiy@bigmir.net>
2021-01-03 07:47:12 +0200

    customized selects with React Select

--- src/components/ui/atoms/Select/Select.jsx
+++ src/components/ui/atoms/Select/Select.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
+import ReactSelect from 'react-select'
+
+import './Select.module.sass'
 
 function Select(props) {
 
+    const options = props.data.map((item, index) => {
+        return {value: item, label: item}
+    })
+
     let changeOption = (e) => {
-        props.changeOption(e.target.value)
+        props.changeOption(e.value)
+    }
+
+    const customStyles = {
+        control: (provided) => ({
+            ...provided,
+            padding: '15px 15px 15px 70px',
+            borderRadius: 25,
+            outline: 'none',
+            border: '1px solid #dadee7!important',
+            boxShadow: 'none!important',
+            cursor: 'pointer'
+        }),
+        valueContainer: (provided) => ({
+            ...provided,
+            padding: 0,
+            margin: 0,
+            width: '100%',
+            fontSize: 14,
+            color: '#000000',
+            lineHeight: 1
+        }),
+        dropdownIndicator: (provided) => ({
+            ...provided,
+            padding: 0,
+        }),
+        indicatorSeparator: () => ({
+            display: 'none'
+        }),
+        placeholder: (provided) => ({
+            ...provided,
+            width: '100%',
+            color: '#000',
+            fontWeight: '500'
+        }),
+        singleValue: (provided) => ({
+            ...provided,
+            width: '100%',
+            color: '#000',
+            fontWeight: '500'
+        })
     }
 
     return (
-        <label className="select">
-            <select onChange={changeOption}>
-                {props.data.map((item, index) => {
-                  return <option value={item} key={index}>{item}</option>
-                })}
-            </select>
+        <label>
+            <span>Period:</span>
+            <ReactSelect
+                options={options}
+                onChange={changeOption}
+                styles={customStyles}
+                defaultValue={options[0]}
+            />
         </label>
+
     );
 }
 
