047d487125488cfa73f173a5e6f0406a5b867cde
alex <alex_kobilianskiy@bigmir.net>
2021-01-13 09:24:09 +0200

    add styles for Circle

--- src/components/Statistics/Circle/Circle.jsx
+++ src/components/Statistics/Circle/Circle.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
 //styles
 import s from './Circle.module.sass'
+import 'react-circular-progressbar/dist/styles.css';
 
 function Circle(props) {
+
+    const percentage = 66;
+
+    let styles = {
+        root: {
+            width: '190px'
+        },
+        path: {
+            transform: 'rotate(0.5turn)',
+            transformOrigin: 'center center',
+            stroke: `red`,
+        },
+        trail: {
+            stroke: '#dadee7',
+            transform: 'rotate(0.5turn)',
+            transformOrigin: 'center center',
+        },
+        text: {
+            fill: 'red',
+            fontSize: '24px',
+        },
+
+    }
+
     return (
         <div className={s.circle}>
-            test
+            <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={styles}
+                strokeWidth='11'
+            />
+            <div className={s.content}>
+                <div className={s.value}>2,300$</div>
+                <div className={s.title}>Direct Sales</div>
+            </div>
         </div>
     );
 }
