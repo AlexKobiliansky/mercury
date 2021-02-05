9e730ade2e7c7b13014637d6c3e85545a9aa278c
alex <alex_kobilianskiy@bigmir.net>
2021-02-05 11:47:33 +0200

    reflow charts on sidebar toggle

--- src/components/Layouts/DefaultLayout/Header/Hamburger/Hamburger.jsx
+++ src/components/Layouts/DefaultLayout/Header/Hamburger/Hamburger.jsx
@@ -3,11 +3,25 @@ import classNames from 'classnames';
 import s from './Hamburger.module.sass';
 import {connect} from 'react-redux';
 import {toggleSidebar} from '../../../../../redux/actions/sidebar';
+import Highcharts from "highcharts";
 
 function Hamburger({openedSidebar, toggleSidebar}) {
+
+
+    let toggleSidebarHandler = () => {
+        toggleSidebar();
+        for (let i = 0; i < Highcharts.charts.length; i++) {
+            if (typeof Highcharts.charts[i] != 'undefined') {
+                setTimeout(function () {
+                    Highcharts.charts[i].reflow();
+                }, 250);
+            }
+        }
+    }
+
     return (
-        <div className={classNames(s.hamburger, {[s.active]: openedSidebar})} onClick={toggleSidebar}>
-            <div className={s.hamburgerInner} />
+        <div className={classNames(s.hamburger, {[s.active]: openedSidebar})} onClick={toggleSidebarHandler}>
+            <div className={s.hamburgerInner}/>
         </div>
     );
 }
